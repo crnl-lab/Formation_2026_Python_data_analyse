599495846b389af9f16510f3681cf7d453c7fee5
Samuel Garcia <sam.garcia.die@gmail.com>
2026-03-13 13:46:55 +0100

    Exo numpy

--- notebooks/ma_bibliotheque.py
+++ notebooks/ma_bibliotheque.py
@@ -9,6 +9,6 @@ def affiche_un_beau_triangle(n: int =10):
 def affiche_un_beau_triangle_tete_en_bas(n: int =10):
     print('_' * (n + 2))
     for i in range(n):
-        print('|' + (' ' * (n - i)) + '/')
+        print('|' + (' ' * (n - i - 1)) + '/')
     
 
